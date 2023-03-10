c59b70f245f8780c2481ca7ca928f33446514c04
Andy Huang <andyex88@gmail.com>
2023-03-10 16:14:35 -0700

    update

--- app.py
+++ app.py
@@ -34,12 +34,15 @@ def callback():
 
     return 'OK'
 
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
+    r = "Please enter specific words"
+
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=msg))
 
 
 if __name__ == "__main__":
